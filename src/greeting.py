4ebfd74d4d78b24fbef60eebc9f62a2737307bfd
Ian Robinson <ian.robinson27@nhs.net>
2024-10-04 16:26:22 +0100

    Test message

--- src/greeting.py
+++ src/greeting.py
@@ -13,4 +13,4 @@ def greeting_message() -> str:
     :return: Greeting
     """
     logger.info(boto3.resource("s3"))
-    return "Hello World! v3"
+    return "This is from a test branch, so not returning the standard hello world."
