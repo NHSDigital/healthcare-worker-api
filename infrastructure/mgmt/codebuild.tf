cebb6299bd85cead65ddec06d01fa721c02fef32
Ian Robinson <ian.robinson27@nhs.net>
2024-10-04 09:56:26 +0100

    Packaging back to NONE

--- infrastructure/mgmt/codebuild.tf
+++ infrastructure/mgmt/codebuild.tf
@@ -61,7 +61,7 @@ resource "aws_codebuild_project" "hcw-api-build" {
 
     name = "hcw-api-build.zip"
     namespace_type = "BUILD_ID"
-    packaging = "ZIP"
+    packaging = "NONE"
   }
 
   source {
