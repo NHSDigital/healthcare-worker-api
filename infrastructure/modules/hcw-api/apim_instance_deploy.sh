e26bd86ff3eb83bd416c341a5c76ded07f777539
Ian Robinson <ian.robinson27@nhs.net>
2024-10-09 11:05:33 +0100

    Fix ls

--- infrastructure/modules/hcw-api/apim_instance_deploy.sh
+++ infrastructure/modules/hcw-api/apim_instance_deploy.sh
@@ -28,7 +28,7 @@ proxygen settings set spec_output_format "yaml"
 
 # Get proxygen private key to allow for proxy instance deployment
 aws secretsmanager get-secret-value --secret-id "$apim_private_key_secret_arn" | jq -r ".SecretString" > /tmp/proxygen_private_key.pem
-ls ~/proxygen_private_key.pem
+ls /tmp/proxygen_private_key.pem
 proxygen credentials set private_key_path /tmp/proxygen_private_key.pem key_id key-1 client_id healthcare-worker-api-client
 echo "Set service credentials"
 
