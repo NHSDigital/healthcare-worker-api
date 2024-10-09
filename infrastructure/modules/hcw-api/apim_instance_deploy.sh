a481ae8645a1004e10eb89311e6466a5af8c267e
Ian Robinson <ian.robinson27@nhs.net>
2024-10-09 09:53:31 +0100

    Set proxygen settings

--- infrastructure/modules/hcw-api/apim_instance_deploy.sh
+++ infrastructure/modules/hcw-api/apim_instance_deploy.sh
@@ -16,6 +16,11 @@ else
   env_name_suffix=""
 fi
 
+#Proxygen settings
+proxygen settings set api "healthcare-worker-api"
+proxygen settings set endpoint_url "https://proxygen.prod.api.platform.nhs.uk"
+proxygen settings set spec_output_format "yaml"
+
 # Get proxygen private key to allow for proxy instance deployment
 aws secretsmanager get-secret-value --secret-id "$apim_private_key_secret_arn" | jq -r ".SecretString" > ~/proxygen_private_key.pem
 proxygen credentials set private_key_path ~/proxygen_private_key.pem key_id key-1 client_id healthcare-worker-api-client
